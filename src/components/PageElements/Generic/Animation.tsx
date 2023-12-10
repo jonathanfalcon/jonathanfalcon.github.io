35f422ac8f4dfedc47558a2321e784a2b4421208
Jonathan Falcon <JQFalcon@outlook.com>
2023-12-09 20:50:05 -0600

    Installed headlessui and removed framer-motion

--- src/components/PageElements/Generic/Animation.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-
-export const Animation = ({ component, children, className }) => {
-    const Component = motion[component]
-
-    return <Component className={className}>{children}</Component>
-}
